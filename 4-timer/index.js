49b1768854ef804b60d9f931d031bb728c2c1683
Lena <>
2023-03-16 18:31:27 +0300

    delete extra code

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -6,19 +6,21 @@ const blockTimer = {
     minutes: document.querySelector('.minutes'),
     seconds: document.querySelector('.seconds'),
     hours: document.querySelector('.hour'),
+
+    words: {
+        months: ['месяц', 'месяца', 'месяцев'],
+        days: ['день', 'дня', 'дней'],
+        minutes: ['минута', 'минуты', 'минут'],
+        hours: ['час', 'часа', 'часов'],
+        seconds: ['секунда', 'секунды', 'секунд']
+    }
 }
 const blockTimer2 = document.querySelector('.timer-ny2');
 const info = document.querySelector('.info');
 
-const words = {
-    month: ['месяц', 'месяца', 'месяцев'],
-    day: ['день', 'дня', 'дней'],
-    minute: ['минута', 'минуты', 'минут'],
-    hour: ['час', 'часа', 'часов'],
-    second: ['секунда', 'секунды', 'секунд']
-}
 
-function num_word(val, words) {
+
+function foldingWord(val, words) {
     const value = Math.abs(Number(val)) % 100;
     const num = value % 10;
     if (value > 10 && value < 20) { return words[2]; }
@@ -34,46 +36,48 @@ function timerNewYear(dateNY) {
         dayNY = new Date(dateNY).getDate();
 
     const interval = setInterval(() => {
-        const monthNow = new Date().getMonth(),
-            now = new Date().getTime(),
-            deltaDates = finish - now,
-            yearNow = new Date().getFullYear(),
+        const now = new Date(),
+            monthNow = now.getMonth(),
+            deltaDates = finish - now.getTime(),
+            yearNow = now.getFullYear(),
+            dayInMonth = (32 - new Date(now.getFullYear(), now.getMonth(), 32).getDate());
 
-            dayInMonth = (32 - new Date(new Date().getFullYear(), new Date().getMonth(), 32).getDate()),
-            deltaMonth = Math.abs(monthNow - monthNY + (11 * (yearNow - yearNY))),
-            deltaDays = (deltaMonth === 0) ? Math.abs(dayNY - new Date().getDate()) : dayInMonth - new Date().getDate(),
+        const timeObj = {
+            months: Math.abs(monthNow - monthNY + (11 * (yearNow - yearNY))),
+            days: (Math.abs(monthNow - monthNY + (11 * (yearNow - yearNY))) === 0) ? Math.abs(dayNY - now.getDate()) : dayInMonth - now.getDate(),
+            hours: Math.floor((deltaDates % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes: Math.floor((deltaDates % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds: Math.floor((deltaDates % (1000 * 60)) / 1000),
+        }
 
-            hours = Math.floor((deltaDates % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-            minutes = Math.floor((deltaDates % (1000 * 60 * 60)) / (1000 * 60)),
-            seconds = Math.floor((deltaDates % (1000 * 60)) / 1000);
-
-        blockTimer.months.innerText = `${deltaMonth} ${num_word(deltaMonth, words.month)}`;
-        blockTimer.days.innerText = `${deltaDays} ${num_word(deltaDays, words.day)}`;
-        blockTimer.minutes.innerText = `${minutes} ${num_word(minutes, words.minute)}`;
-        blockTimer.seconds.innerText = `${seconds} ${num_word(seconds, words.second)}`;
-        blockTimer.hours.innerText = `${hours} ${num_word(hours, words.hour)}`;
+        for (const [key] of Object.entries(blockTimer)) {
+            if (key !== 'words') {
+                blockTimer[key].innerText = `${timeObj[key]} ${foldingWord(timeObj[key], blockTimer.words[key])}`;
+            }
+        }
 
         blockTimer2.innerText = `${new Intl.DateTimeFormat('en-US', {
+            month: "numeric",
+            day: "numeric",
             hour: "numeric",
             minute: "numeric",
             second: "numeric",
-            day: "numeric",
-            month: "numeric",
-        }).formatToParts(new Date(yearNow, deltaMonth - 1, deltaDays, hours, minutes, seconds)).map(({ type, value }) => {
-            switch (type) {
-                case 'month': return `${value} ${num_word(value, words.month)}`;
-                    break;
-                case 'day': return `${value} ${num_word(value, words.day)}`;
-                    break;
-                case 'minute': return `${value} ${num_word(value, words.minute)}`;
-                    break
-                case 'hour': return `${value} ${num_word(value, words.hour)}`;
-                    break;
-                case 'second': return `${value} ${num_word(value, words.second)}`;
-                    break;
-                default: return value;
-            }
-        }).reduce((string, part) => string + part)
+        }).formatToParts(new Date(yearNow, timeObj.months - 1, timeObj.days, timeObj.hours, timeObj.minutes, timeObj.seconds))
+            .map(({ type, value }) => {
+                switch (type) {
+                    case 'month': return `${value} ${foldingWord(value, blockTimer.words.months)}`;
+                        break;
+                    case 'day': return `${value} ${foldingWord(value, blockTimer.words.days)}`;
+                        break;
+                    case 'minute': return `${value} ${foldingWord(value, blockTimer.words.minutes)}`;
+                        break
+                    case 'hour': return `${value} ${foldingWord(value, blockTimer.words.hours)}`;
+                        break;
+                    case 'second': return `${value} ${foldingWord(value, blockTimer.words.seconds)}`;
+                        break;
+                    default: return value;
+                }
+            }).reduce((string, part) => string + part)
             }`
 
         if (deltaDates <= 0) {
