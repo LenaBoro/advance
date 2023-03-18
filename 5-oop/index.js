64d84b5eaa2fd3ea1d30cd5cf9b9f055b22f3b9b
Lena <>
2023-03-18 16:32:32 +0300

    fix letters

--- 5-oop/index.js
+++ 5-oop/index.js
@@ -12,15 +12,17 @@ Character.prototype.sayHi = function () {
     console.log(lang, name);
 }
 
+/* ORK */
+
 const Ork = function (obj) {
     Character.apply(this, arguments);
-    const { weapon } = obj
+    const { weapon } = obj;
     this.weapon = weapon;
 }
 Ork.prototype = Object.create(Character.prototype);
 Ork.prototype.constructor = Ork;
 
-
+/* ELF */
 const Elf = function () {
     Character.apply(this, arguments);
     this.spell = [];
@@ -33,10 +35,10 @@ Elf.prototype.createSpell = function (stringSpell) {
     console.log('new spell ' + stringSpell);
 }
 
-const ork1 = new Ork({ name: 'Semen', lang: 'en', race: 'ork' });
+const ork1 = new Ork({ name: 'Semen', lang: 'ork\'s toungue', race: 'ork', weapon: 'axe' });
 const elf1 = new Ork({ name: 'Nemes', lang: 'elf\'s toungue', race: 'elf' });
 
 ork1.sayHi();
 elf1.sayHi();
 
-elf1.createspell('give me food');
+elf1.createSpell('give me food');
